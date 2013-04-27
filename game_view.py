fc69c284fd83259115fd75f61978ddae97182e2b
Tom Gooding <tom.pudding@gmail.com>
2013-04-27 08:46:06 +0100

    Initial commit of skeleton code from game library

---
+++ game_view.py
@@ -0,0 +1,56 @@
+from OpenGL.GL import *
+import random,numpy,cmath,math,pygame
+
+import ui,globals,drawing,os,copy
+from globals.types import Point
+import modes
+import random
+
+class GameView(ui.RootElement):
+    def __init__(self):
+        self.atlas = globals.atlas = drawing.texture.TextureAtlas('tiles_atlas_0.png','tiles_atlas.txt')
+        self.game_over = False
+        #pygame.mixer.music.load('music.ogg')
+        #self.music_playing = False
+        super(GameView,self).__init__(Point(0,0),globals.screen)
+        self.mode = modes.Titles(self)
+        self.StartMusic()
+
+    def StartMusic(self):
+        pass
+        #pygame.mixer.music.play(-1)
+        #self.music_playing = True
+
+    def Draw(self):
+        drawing.ResetState()
+        drawing.DrawAll(globals.backdrop_buffer,self.atlas.texture.texture)
+        drawing.ResetState()
+        drawing.DrawAll(globals.quad_buffer,self.atlas.texture.texture)
+        drawing.DrawAll(globals.nonstatic_text_buffer,globals.text_manager.atlas.texture.texture)
+        
+    def Update(self,t):
+        if self.mode:
+            self.mode.Update(t)
+
+        if self.game_over:
+            return
+            
+        self.t = t
+
+    def GameOver(self):
+        self.game_over = True
+        self.mode = modes.GameOver(self)
+        
+    def KeyDown(self,key):
+        self.mode.KeyDown(key)
+
+    def KeyUp(self,key):
+        if key == pygame.K_DELETE:
+            if self.music_playing:
+                self.music_playing = False
+                pygame.mixer.music.set_volume(0)
+            else:
+                self.music_playing = True
+                pygame.mixer.music.set_volume(1)
+        self.mode.KeyUp(key)
+
---
+++ modes.py
@@ -0,0 +1,144 @@
+from OpenGL.GL import *
+import random,numpy,cmath,math,pygame
+
+import ui,globals,drawing,os,copy
+from globals.types import Point
+import sys
+
+class Mode(object):
+    """ Abstract base class to represent game modes """
+    def __init__(self,parent):
+        self.parent = parent
+    
+    def KeyDown(self,key):
+        pass
+    
+    def KeyUp(self,key):
+        pass
+
+    def MouseButtonDown(self,pos,button):
+        return False,False
+
+    def Update(self,t):
+        pass
+
+class TitleStages(object):
+    STARTED  = 0
+    COMPLETE = 1
+    TEXT     = 2
+    SCROLL   = 3
+    WAIT     = 4
+
+class Titles(Mode):
+    blurb = "ORDINAL"
+    def __init__(self,parent):
+        self.parent          = parent
+        self.start           = None
+        self.stage           = TitleStages.STARTED
+        self.handlers        = {TitleStages.STARTED  : self.Startup,
+                                TitleStages.COMPLETE : self.Complete}
+        bl = self.parent.GetRelative(Point(0,0))
+        tr = bl + self.parent.GetRelative(globals.screen)
+        self.blurb_text = ui.TextBox(parent = self.parent,
+                                     bl     = bl         ,
+                                     tr     = tr         ,
+                                     text   = self.blurb ,
+                                     textType = drawing.texture.TextTypes.GRID_RELATIVE,
+                                     colour = (1,1,1,1),
+                                     scale  = 4)
+        self.backdrop        = ui.Box(parent = globals.screen_root,
+                                      pos    = Point(0,0),
+                                      tr     = Point(1,1),
+                                      colour = (0,0,0,0))
+        self.backdrop.Enable()
+
+    def KeyDown(self,key):
+        self.stage = TitleStages.COMPLETE
+
+    def Update(self,t):
+        if self.start == None:
+            self.start = t
+        
+        self.elapsed = t - self.start
+        self.stage = self.handlers[self.stage](t)
+
+    def Complete(self,t):
+        self.backdrop.Delete()
+        self.blurb_text.Delete()
+        self.parent.mode = GameOver(self.parent)
+        
+
+    def Startup(self,t):
+        return TitleStages.STARTED
+
+class GameOver(Mode):
+    blurb = "GAME OVER"
+    def __init__(self,parent):
+        self.parent          = parent
+        self.blurb           = self.blurb
+        self.blurb_text      = None
+        self.handlers        = {TitleStages.TEXT    : self.TextDraw,
+                                TitleStages.SCROLL  : self.Wait,
+                                TitleStages.WAIT    : self.Wait}
+        self.backdrop        = ui.Box(parent = globals.screen_root,
+                                      pos    = Point(0,0),
+                                      tr     = Point(1,1),
+                                      colour = (0,0,0,0.6))
+        
+        bl = self.parent.GetRelative(Point(0,0))
+        tr = bl + self.parent.GetRelative(globals.screen)
+        self.blurb_text = ui.TextBox(parent = globals.screen_root,
+                                     bl     = bl         ,
+                                     tr     = tr         ,
+                                     text   = self.blurb ,
+                                     textType = drawing.texture.TextTypes.SCREEN_RELATIVE,
+                                     scale  = 3)
+
+        self.start = None
+        self.blurb_text.EnableChars(0)
+        self.stage = TitleStages.TEXT
+        self.played_sound = False
+        self.skipped_text = False
+        self.letter_duration = 20
+        self.continued = False
+        #pygame.mixer.music.load('end_fail.mp3')
+        #pygame.mixer.music.play(-1)
+
+    def Update(self,t):
+        if self.start == None:
+            self.start = t
+        self.elapsed = t - self.start
+        self.stage = self.handlers[self.stage](t)
+        if self.stage == TitleStages.COMPLETE:
+            raise sys.exit('Come again soon!')
+
+    def Wait(self,t):
+        return self.stage
+
+    def SkipText(self):
+        if self.blurb_text:
+            self.skipped_text = True
+            self.blurb_text.EnableChars()
+
+    def TextDraw(self,t):
+        if not self.skipped_text:
+            if self.elapsed < (len(self.blurb_text.text)*self.letter_duration) + 2000:
+                num_enabled = int(self.elapsed/self.letter_duration)
+                self.blurb_text.EnableChars(num_enabled)
+            else:
+                self.skipped_text = True
+        elif self.continued:
+            return TitleStages.COMPLETE
+        return TitleStages.TEXT
+
+
+    def KeyDown(self,key):
+        #if key in [13,27,32]: #return, escape, space
+        if not self.skipped_text:
+            self.SkipText()
+        else:
+            self.continued = True
+
+    def MouseButtonDown(self,pos,button):
+        self.KeyDown(0)
+        return False,False
